db8e6aa0019d0b93085c21b9d4da5dcc95acd7af
RomanBorr <starmordar@gmail.com>
2019-09-25 20:15:57 +0300

    added second FormStep to page

--- frontend/src/App/components/WorspaceForm/ChooseTechnologies/ChooseTechnologies.js
+++ frontend/src/App/components/WorspaceForm/ChooseTechnologies/ChooseTechnologies.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ChooseTechnologies.css';
 
+import FilterArea from '../../FilterArea/FilterArea';
+
 class ChooseTechnologies extends React.Component {
     constructor(props) {
         super(props)
@@ -20,6 +22,45 @@ class ChooseTechnologies extends React.Component {
                     <BrowserCard />
                     <BrowserCard />
                 </div>
+
+                <div className="techno-selection">
+                    <FilterArea />
+
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                    <TechnoCard />
+                </div>
+
+                <div className='btn-container'>
+                    <button type="submit" class="btn btn-success">Create Workspaces</button>
+                </div>
             </div>
         )
     }
@@ -37,6 +78,23 @@ class BrowserCard extends React.Component {
                 <div className="card-border">
                     <img src="https://cdn0.iconfinder.com/data/icons/jfk/512/chrome-512.png" width="140px" height="140px"></img>
                     <h5 className="browser-description"> Google Chrome</h5>
+                </div>
+            </div>
+        )
+    }
+}
+
+class TechnoCard extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="techno-card col-3">
+                <div className="card-border techno-card-border">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7b/Icon_Atom.svg" width="80px" height="80px"></img>
+                    <h5 className="techno-description">Atom</h5>
                 </div>
             </div>
         )
