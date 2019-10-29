b22a5b963bc9151210b2bb80603af4a464892be5
RomanBorr <starmordar@gmail.com>
2019-10-30 02:42:25 +0300

    Updates

---
+++ precache-manifest.c70720c80b2b4e3a27fdf8b8505d1dce.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94939279d412220b200d8fc4b544812b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d222bd6065e8a0823175",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6736ea237a94369a5850",
+    "url": "/static/css/main.c44b760b.chunk.css"
+  },
+  {
+    "revision": "d222bd6065e8a0823175",
+    "url": "/static/js/2.54567874.chunk.js"
+  },
+  {
+    "revision": "6736ea237a94369a5850",
+    "url": "/static/js/main.170243d1.chunk.js"
+  },
+  {
+    "revision": "65a81b1ca9a7620b8ceb",
+    "url": "/static/js/runtime-main.1dc4db23.js"
+  },
+  {
+    "revision": "0dfd203155436aacae5ff076d08cadfe",
+    "url": "/static/media/IE.0dfd2031.png"
+  },
+  {
+    "revision": "8c437dd1243598e6954ad539d8fc32ae",
+    "url": "/static/media/VScode.8c437dd1.svg"
+  },
+  {
+    "revision": "12cae70b88c1478897484aeb6c0dd11d",
+    "url": "/static/media/atom.12cae70b.jpg"
+  },
+  {
+    "revision": "14e0561947a73bf44f4b2b513a7b5139",
+    "url": "/static/media/eye.14e05619.png"
+  },
+  {
+    "revision": "f1d6ef7d63f5d46f49cb3c22ca57b0ef",
+    "url": "/static/media/firefox.f1d6ef7d.png"
+  },
+  {
+    "revision": "451805cb4eab5fc709d6a6c73cb02fd8",
+    "url": "/static/media/google.451805cb.png"
+  },
+  {
+    "revision": "44b1a5715b84301b12e9abab1557ebca",
+    "url": "/static/media/octopus.44b1a571.png"
+  },
+  {
+    "revision": "7aa98998bf4db4244bf3ed36d31e7bee",
+    "url": "/static/media/sublime.7aa98998.png"
+  },
+  {
+    "revision": "4a3f72b73f536c91eb377cb7f73753f9",
+    "url": "/static/media/wall.4a3f72b7.png"
+  }
+]);
